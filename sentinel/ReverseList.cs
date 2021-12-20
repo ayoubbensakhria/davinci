2743f02484b45eb5f010ef09d5440843cb888795
Ayoub <bensakhria.ayoub@gmail.com>
2021-12-20 22:02:08 +0100

    add recursive approach

--- sentinel/ReverseList.cs
+++ sentinel/ReverseList.cs
@@ -33,3 +33,31 @@ public class Solution {
         return newHead;             
     }
 }
+
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     public int val;
+ *     public ListNode next;
+ *     public ListNode(int val=0, ListNode next=null) {
+ *         this.val = val;
+ *         this.next = next;
+ *     }
+ * }
+ */
+
+ // ----------------------------------------------------------------
+ // Solution 2 - recursion
+ // ----------------------------------------------------------------
+public class Solution {
+    public ListNode ReverseList(ListNode head) {
+        // recursion from newHead == null
+        return RecursiveReverseList(head, null);                   
+    }
+    public ListNode RecursiveReverseList(ListNode head, ListNode newHead){
+        if(head == null) return newHead;
+        ListNode next = head.next;
+        head.next = newHead;
+        return RecursiveReverseList(next, head);
+    }
+}
--- sentinel/ReverseList.py
+++ sentinel/ReverseList.py
@@ -12,3 +12,21 @@ class Solution:
             newHead = head
             head = next
         return newHead
+
+#----------------------------------------------------------------
+# Solution 2 - recursion
+#----------------------------------------------------------------
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def reverseList(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        return self.reverseListRecursion(head, None)
+    def reverseListRecursion(self, head: Optional[ListNode], newHead: Optional[ListNode]) -> Optional[ListNode]:
+        if not head:
+            return newHead
+        next = head.next
+        head.next = newHead
+        return self.reverseListRecursion (next, head)
