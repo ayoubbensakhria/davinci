84f951e0c528a85210bd9f9cb015391db30d1963
Ayoub <bensakhria.ayoub@gmail.com>
2021-12-26 02:43:11 +0100

    add python and c# optimal O(N) for twoSum

--- pointers/TwoSum.cs
+++ pointers/TwoSum.cs
@@ -15,3 +15,25 @@ public class Solution {
         return indexes;
     }
 }
+
+//-------------------------------
+// Faster solution using dictionary
+//-------------------------------
+public class Solution {
+    public int[] TwoSum(int[] nums, int target) {
+        // there is exactly one solution
+        // dict[target - i] = i example: dict[9-2] = 0
+        // if (exists) dict[i] 
+        // the pair is {dict[nums[i]], i}  example dict[7], and nums[0] = 2
+        Dictionary<int, int> dict = new Dictionary<int, int>();
+        for (int i=0; i<nums.Length; i++){
+            if(dict.ContainsKey(nums[i])){
+                return new int[] {dict[nums[i]], i};
+            } else {
+                dict[target-nums[i]] = i;
+            }
+        }
+    // if no pairs
+    return new int []{};
+    }
+}
--- pointers/TwoSum.py
+++ pointers/TwoSum.py
@@ -4,3 +4,17 @@ class Solution:
             for j in range (i+1, len(nums)):
                 if (nums[i] + nums[j] == target):
                     return [i, j]
+
+##--------------------------------
+## Faster solution using dictionary
+##--------------------------------
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        dict = {}
+        for i in range (len(nums)):
+            if(nums[i] in dict):
+                return [dict[nums[i]], i]
+            else:
+                dict[target-nums[i]] = i
+        # if no pairs
+        return []
