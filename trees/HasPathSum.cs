8dd2d348fe01a61c93f89c7bbac5c3695f8f77ea
Ayoub <bensakhria.ayoub@gmail.com>
2022-01-24 21:08:14 +0100

    add recursive DFS solution hasPathSum in c# and python

---
+++ trees/HasPathSum.cs
@@ -0,0 +1,25 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     public int val;
+ *     public TreeNode left;
+ *     public TreeNode right;
+ *     public TreeNode(int val=0, TreeNode left=null, TreeNode right=null) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+public class Solution {
+    public bool HasPathSum(TreeNode root, int targetSum) {
+        if(root == null) return false;
+        // if sum is the value of the node itself
+        if(root.left == null && root.right == null && root.val == targetSum) return true;
+        // Decriment target sum by root.val till reaching 0
+        targetSum -= root.val;
+        // return true if recursion is true from left or right
+        return HasPathSum(root.left, targetSum) || HasPathSum(root.right, targetSum);
+        
+    }
+}
