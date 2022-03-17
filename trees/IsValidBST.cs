7356d452fbbfc8bb5c7b33ca0626b49e411f6662
Ayoub <bensakhria.ayoub@gmail.com>
2022-03-17 23:07:29 +0100

    add explanations to isValidBST

--- trees/IsValidBST.cs
+++ trees/IsValidBST.cs
@@ -18,21 +18,23 @@ public class Solution {
     public bool IsValidBST(TreeNode root) {
         // true for void treenodes
         if (root == null) return true;
-        // initialize previous to store previous nodes
-        // while traversing the tree
         TreeNode previous = null;
-        // push nodes in stack
+        // push nodes in stack (InOrder Traversal)
         Stack<TreeNode> stack = new Stack<TreeNode>();
+        // perform inorder traversal
+        // 2 
+        // 1 3
         while(root != null || stack.Any()){
             while(root != null){
                 stack.Push(root);
                 root = root.left;
             }
-            root = stack.Pop();
-            // previous val must be smaller than root
+            // get the top element
+            root = stack.Pop(); 
+            // previous val must be smaller than root val
             if(previous != null && root.val <= previous.val) return false;
-            previous = root;
-            root = root.right;
+            previous = root; 
+            root = root.right; 
         }
         // if all fine
         return true;
