67638916390e2389137e57c65dc339a8827c1d5d
Ayoub <bensakhria.ayoub@gmail.com>
2022-03-22 23:08:05 +0100

    add explanations to 3 dynamic programming problems

--- dynamic_programming/MaxProfit.cs
+++ dynamic_programming/MaxProfit.cs
@@ -1,9 +1,10 @@
 public class Solution {
     public int MaxProfit(int[] prices) {
-        //Kadane's Algorithm
         int max = 0, min = Int32.MaxValue;
         foreach(int price in prices) {
+            // find the min price
             min = Math.Min(price, min);
+            // find max profit (gap)
             max = Math.Max(price-min, max);
         }
         return max;
